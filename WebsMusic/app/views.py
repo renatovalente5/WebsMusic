c3009f1b70c2b142b2cf63fad5c2468f73f61fbd
Pedro Valente <pedro.valente@ua.pt>
2020-12-27 23:26:40 +0000

    criarPlaylist a funfar

--- WebsMusic/app/views.py
+++ WebsMusic/app/views.py
@@ -11,6 +11,7 @@ from lxml import etree
 import xmltodict
 # import requests
 import random
+import datetime
 
 # from lxml import etree
 # from urllib.request import urlopen
@@ -183,6 +184,7 @@ def albums(request):
         temp['recorder'] = unquote(a['recorder']['value'])
         temp['data'] = a['data']['value']
         temp['streams'] = a['count']['value']
+        temp['nome'] = a['albumName']['value']
         info[unquote(a['albumName']['value'])] = temp
         # info[a['id']['value']] = unquote(a['aname']['value'])
 
@@ -193,80 +195,238 @@ def albums(request):
     return render(request, "albums.html", tparams)
 
 
+def albumInfo(request):
+    id = str(request.GET.get('id'))
+    print(request.GET)
+    tparams = dict()
+    query_BasicInfo = '''PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                            PREFIX cs: <http://www.xpand.com/rdf/>
+                            PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+                            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+
+                            select ?album ?idArt ?aname ?wiki ?bio ?data ?genero ?prevAlbum ?nextAlbum ?count ?img ?recorder ?producer
+                            where {
+                                ?album foaf:name "%s" .
+                                ?album rdf:type cs:Album .
+                                ?album cs:MusicArtist ?idArt .
+                                ?idArt foaf:name ?aname .
+                                ?album cs:WikiData ?wiki .
+                                ?album cs:biography ?bio .
+                                ?album cs:datePublished ?data .
+                                ?album cs:genre ?genero .
+                                optional{
+                                    ?album cs:previousAlbum ?prevAlbum .
+                                }
+                                optional{
+                                    ?album cs:nextAlbum ?nextAlbum .
+                                }
+                                ?album cs:playCount ?count .
+                                optional{
+                                     ?album cs:producer ?producer .
+                                }
+                            #    ?album cs:similarAlbum ?simAlbum .
+                                ?album foaf:Image ?img .
+
+                            }''' % (quote(id))
+    _body = {"query": query_BasicInfo}
+    res = accessor.sparql_select(body=_body, repo_name=_repositorio)
+    res = json.loads(res)
+    res = res['results']['bindings'][0]
+    print(res)
+    tparams['name'] = id
+    tparams['uri'] = res['album']['value']
+    tparams['idArtista'] = res['idArt']['value']
+    tparams['aname'] = unquote(res['aname']['value'])
+    tparams['img'] = res['img']['value']
+    tparams['bio'] = unquote(res['bio']['value'])
+    tparams['data'] = res['data']['value']
+    tparams['genero'] = unquote(res['genero']['value'])
+    tparams['playCount'] = res['count']['value']
+    tparams['prevAlbum'] = None
+    tparams['nextAlbum'] = None
+    tparams['producer'] = None
+    print(res.keys())
+    if ('prevAlbum' in res.keys()):
+        tparams['prevAlbum'] = unquote(res['prevAlbum']['value'])
+    if ('nextAlbum' in res.keys()):
+        tparams['nextAlbum'] = unquote(res['nextAlbum']['value'])
+    if ('producer' in res.keys()):
+        tparams['producer'] = unquote(res['producer']['value'])
+
+    query_Tags = '''PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                        PREFIX cs: <http://www.xpand.com/rdf/>
+                        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+                        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+
+                        select ?idTag ?tag 
+                        where {
+                            ?album foaf:name "%s" .
+                            ?album rdf:type cs:Album .
+                            ?album cs:Tag ?idTag .
+                            ?idTag foaf:name ?tag .
+                        }
+                        ''' % (quote(id))
+
+    _body = {"query": query_Tags}
+    res = accessor.sparql_select(body=_body, repo_name=_repositorio)
+    res = json.loads(res)
+
+    tags = dict()
+    for t in res['results']['bindings']:
+        tags[unquote(t['tag']['value'])] = t['idTag']['value']
+    tparams['tags'] = tags
+
+    query_simAlbums = '''PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                        PREFIX cs: <http://www.xpand.com/rdf/>
+                        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+                        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+
+                        select ?simAlbum ?name
+                        where {
+                            ?album foaf:name "%s" .
+                            ?album rdf:type cs:Album .
+                            ?album cs:similarAlbum ?simAlbum .
+                            ?simAlbum foaf:name ?name .
+                        } ''' % (quote(id))
+
+    _body = {"query": query_simAlbums}
+    res = accessor.sparql_select(body=_body, repo_name=_repositorio)
+    res = json.loads(res)
+
+    simAlbums = dict()
+    for a in res['results']['bindings']:
+        temp = dict()
+        temp['uri'] = a['simAlbum']['value']
+        temp['id'] = a['name']['value']
+        simAlbums[unquote(a['name']['value'])] = temp
+    tparams['simAlbums'] = simAlbums
+
+    query_recorders = '''PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                            PREFIX cs: <http://www.xpand.com/rdf/>
+                            PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+                            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+
+                            select ?recorder
+                            where {
+                                ?album foaf:name "%s" .
+                                ?album rdf:type cs:Album .
+                                ?album cs:recorder ?recorder .
+
+                            }  ''' % (quote(id))
+
+    _body = {"query": query_recorders}
+    res = accessor.sparql_select(body=_body, repo_name=_repositorio)
+    res = json.loads(res)
+
+    recorders = []
+    for r in res['results']['bindings']:
+        recorders.append(unquote(r['recorder']['value']))
+    tparams['recorders'] = recorders
+
+    return render(request, "albumRDFa.html", tparams)
+
+
 def criarPlayList(request):
-    return None
-    # lastID = "xquery import module namespace funcsPlaylist = 'com.funcsPlaylist.my.index'; funcsPlaylist:last-playlistID()"
-    # query = session.execute(lastID)
-    #
-    # # Verifica se esta alguma playlist na BD, se nao coloca o primeiro id a 1
-    # if query != '':
-    #     res = xmltodict.parse(query)
-    #     id = str(int(res["id"]) + 1)
-    # else:
-    #     id = "1"
-    #
-    # input = "xquery import module namespace funcsPlaylist = 'com.funcsPlaylist.my.index'; funcsPlaylist:musicas()"
-    # query = session.execute(input)
-    # # print(query)
-    # info = dict()
-    # res = xmltodict.parse(query)
-    #
-    # for c in res["root"]["elem"]:
-    #     info[c["name"]] = dict()
-    #     info[c["name"]]["url"] = c["spotify"]
-    #     info[c["name"]]["id"] = c["id"]
-    #     info[c["name"]]["imagem"] = c["url"][2]
-    #     info[c["name"]]["artistas"] = dict()
-    #     if isinstance(c["artista"], list):
-    #         for art in c["artista"]:
-    #             info[c["name"]]["artistas"][art["name"]] = art["id"]
-    #     else:
-    #         info[c["name"]]["artistas"][c["artista"]["name"]] = c["artista"]["id"]
-    # #print(request.POST)
-    #
-    # if 'playlistName' in request.POST:
-    #     nomes = request.POST.getlist('nameMusica')
-    #     playlistNome = request.POST['playlistName']
-    #     print(len(nomes))
-    #     if len(nomes) == 0 or playlistNome == "":
-    #         tparams = {
-    #             'artistas': True,
-    #             'tracks': info,
-    #             'frase': "Songs from Playlist Pokémon LoFi:",
-    #             'erro': True
-    #         }
-    #         return render(request, "criarPlayList.html", tparams)
-    #
-    #     musicas = dict()
-    #     for nMusicas in nomes:
-    #         print(nMusicas)
-    #         input = "xquery import module namespace funcsPlaylist = 'com.funcsPlaylist.my.index'; funcsPlaylist:info-musica('{}')".format(
-    #             nMusicas)
-    #         query = session.execute(input)
-    #         res = xmltodict.parse(query)
-    #         print(res["root"]["elem"])
-    #         musica = res["root"]["elem"]
-    #         musicas[musica["name"]] = dict()
-    #         musicas[musica["name"]]["id"] = musica["id"]
-    #         musicas[musica["name"]]["externalUrl"] = musica["spotify"]
-    #         musicas[musica["name"]]["img"] = musica["url"]
-    #         musicas[musica["name"]]["artistas"] = dict()
-    #         if isinstance(musica["artista"], list):
-    #             for art in musica["artista"]:
-    #                 musicas[musica["name"]]["artistas"][art["name"]] = art["id"]
-    #         else:
-    #             musicas[musica["name"]]["artistas"][musica["artista"]["name"]] = musica["artista"]["id"]
-    #
-    #     criarxml(id, playlistNome, str(len(nomes)), musicas)
-    #     return redirect(myPlayList)
-    # else:
-    #     tparams = {
-    #         'artistas': True,
-    #         'tracks': info,
-    #         'frase': "Songs from Playlist Pokémon LoFi:",
-    #         'erro': False
-    #     }
-    #     return render(request, "criarPlayList.html", tparams)
+    query = '''PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                    PREFIX cs: <http://www.xpand.com/rdf/>
+                    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+                    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+
+                    select ?id ?tname ?aname
+                    where { 
+                        ?id rdf:type cs:Track .
+                        ?id foaf:name ?tname .
+                        ?id cs:MusicArtist ?artist .
+                        ?artist foaf:name ?aname
+                    }'''
+
+    _body = {"query": query}
+    res = accessor.sparql_select(body=_body, repo_name=_repositorio)
+    res = json.loads(res)
+    # print(res);
+    info = dict()
+
+    for m in res['results']['bindings']:
+        info[unquote(m['tname']['value'])] = dict()
+        info[unquote(m['tname']['value'])]["artista"] = unquote(m['aname']['value'])
+        info[unquote(m['tname']['value'])]["id"] = m['id']['value']
+
+    if 'playlistName' in request.POST:
+        nomes = request.POST.getlist('nameMusica')
+        playlistNome = request.POST['playlistName']
+        # print(len(nomes))
+        print(nomes)
+        if len(nomes) == 0 or playlistNome == "":
+            tparams = {
+                'tracks': info,
+                'frase': "Songs:",
+                'erro': True
+            }
+            return render(request, "criarPlayList.html", tparams)
+        else:
+            tparams = {
+                'tracks': info,
+                'frase': "Songs:",
+                'erro': False
+            }
+
+            querysIDS = """
+                        PREFIX cs: <http://www.xpand.com/rdf/>
+                        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                        select * where { 
+                            ?p rdf:type cs:Playlist
+                        } 
+                    """
+            _body = {"query": querysIDS}
+            res = accessor.sparql_select(body=_body, repo_name=_repositorio)
+            res = json.loads(res)
+
+
+            id = "http://www.xpand.com/playlist/" + playlistNome
+
+            for i in res['results']['bindings']:
+                if id == i['p']['value']:
+                    tparams = {
+                        'tracks': info,
+                        'frase': "Songs:",
+                        'erro': True
+                    }
+                    return render(request, "criarPlayList.html", tparams)
+
+            query_insert = """
+                            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                            PREFIX cs: <http://www.xpand.com/rdf/>
+                            PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+
+                            insert data {
+                                <%s> rdf:type cs:Playlist .
+                                <%s> foaf:name "%s" .
+                                <%s> cs:NumItems "%s" .
+                                <%s> cs:datePublished "%s"
+                            }
+                            """ % (id, id, playlistNome, id, len(nomes), id, str(datetime.date.today()))
+            _body = {"update": query_insert}
+            res1 = accessor.sparql_update(body=_body, repo_name=_repositorio)
+
+            for musica in nomes:
+                query_de_insert = """
+                                PREFIX cs: <http://www.xpand.com/rdf/>
+                                insert data {<%s> cs:Track <%s>}
+                            """ % (id, musica)
+                print(musica)
+                print(id)
+                # listArtistas.append(a['outras']['value'])
+                _body = {"update": query_de_insert}
+                res1 = accessor.sparql_update(body=_body, repo_name=_repositorio)
+
+            return render(request, "criarPlayList.html", tparams)
+    else:
+        tparams = {
+            'tracks': info,
+            'frase': "Songs",
+            'erro': False
+        }
+        return render(request, "criarPlayList.html", tparams)
 
 
 def insertKnowsArtist(artista):
@@ -289,17 +449,17 @@ def insertKnowsArtist(artista):
     res = json.loads(res)
     print(res)
 
-    #listArtistas = []
+    # listArtistas = []
     art = res['results']['bindings'][0]['banda']['value']
+    print(art)
     for a in res['results']['bindings']:
         if a['outras']['value'] != art:
             query_insert = """
                         insert data {<%s> foaf:knows <%s>}
                     """ % (art, a['outras']['value'])
-            #listArtistas.append(a['outras']['value'])
+            # listArtistas.append(a['outras']['value'])
             _body = {"update": query_insert}
             res1 = accessor.sparql_update(body=_body, repo_name=_repositorio)
-
 
 
 def knowArtists(request):
